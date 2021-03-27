3809f3f8da4f12414db84a47ec48f9f2506ec1c0
javadebadi <javad.ebadi.1990@gmail.com>
2021-03-27 17:42:22 +0430

    Upated settings to permit communication with angular and using rest framework

--- backend/backend_project/settings.py
+++ backend/backend_project/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # 3rd party apps
+    'corsheaders',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +121,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# add angular server in localhost as a whitelist
+CORS_ORIGIN_WHITELIST = [
+     'http://localhost:4200',
+]
