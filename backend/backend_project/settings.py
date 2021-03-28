12bb7967eb532d9cebcf77dfcc4a834e6a41274e
javadebadi <javad.ebadi.1990@gmail.com>
2021-03-28 19:03:57 +0430

    Updated project using operate.py create from command line

--- backend/backend_project/settings.py
+++ backend/backend_project/settings.py
@@ -16,7 +16,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
@@ -27,7 +26,6 @@ SECRET_KEY = 'jhbvr-1&48kmbz^#9o4_gz0ekcs#b^l^n=9em310c#*9_3o(^^'
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -73,7 +71,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'backend_project.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
@@ -83,7 +80,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
@@ -103,7 +99,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
@@ -117,7 +112,6 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
@@ -127,3 +121,5 @@ STATIC_URL = '/static/'
 CORS_ORIGIN_WHITELIST = [
      'http://localhost:4200',
 ]
+
+FRONTEND_ROOT = os.path.abspath(os.path.join(BASE_DIR, "..", "frontend", "dist", "frontend"))
--- backend/backend_project/urls.py
+++ backend/backend_project/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import include
+from django.urls import re_path
+from django.conf.urls.static import static
+from django.conf.urls.static import serve
+from backend_project import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    re_path(r'^(?P<path>.*)$', serve, {'document_root': settings.FRONTEND_ROOT}),
 ]
